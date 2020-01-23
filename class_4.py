3d190ac3f60527dfa5e70556f7f6d19495db9290
Emma Dahl <emmadahl@Emmas-MacBook-Pro.local>
2020-01-23 14:59:51 +0100

    adding temp file

--- class_4.py
+++ class_4.py
@@ -3,21 +3,10 @@
 Spyder Editor
 
 This is a temporary script file.
+
 """
 
-#integer
-4
-
-#making cells to execute independently
-#%% this makes a cell 
-
-#%% this makes another cell
-
-3.5
-#floating point numbers: have a fractional part
-3.14
-
-#making a string
-'this is a string'
-"this is another string"
-"this is how to deal with \"quotes\" in a string"
+def func (a, b):
+    return a + b
+    
+    
